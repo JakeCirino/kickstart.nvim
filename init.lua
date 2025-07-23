d7268b0e4c12ba66d09cd777abf74f44f0fe31d9
JakeCirino <jakecirino@gmail.com>
2025-07-22 21:04:58 -0500

    Add explorer

--- init.lua
+++ init.lua
@@ -254,7 +254,29 @@ require('lazy').setup({
       require('guess-indent').setup {}
     end,
   }, -- Detect tabstop and shiftwidth automatically
-
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = false },
+      dashboard = { enabled = false },
+      explorer = { enabled = true },
+      indent = { enabled = false },
+      input = { enabled = false },
+      picker = { enabled = false },
+      notifier = { enabled = false },
+      quickfile = { enabled = false },
+      scope = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      words = { enabled = false },
+    },
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
